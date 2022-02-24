a829279b7556576ace79d23693d5a90f97da08d5
TheColfox <connorcam123@gmail.com>
2022-02-24 00:52:26 +0000

    Finished ProjectThumbnails and added grid

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
+import { Box, Typography } from '@mui/material/';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 import HomeAnimation from './components/HomeAnimation';
 import NavHeader from './components/NavHeader';
 import ProjectThumbnail from './components/ProjectThumbnail';
+
+import AcademicPaperLibrary from './img/academicpaperlibrary.png'
+import WildfireDetection from './img/wildfiredetection.png'
+import FutoshikiGeneration from './img/futoshikigeneration.png'
+import HandwrittenDigitRecognition from './img/handwrittendigitrecognition.png'
+
 
 const THEME = createTheme({
   typography: {
@@ -18,19 +26,13 @@ const THEME = createTheme({
    "fontWeightLight": 300,
    "fontWeightRegular": 400,
    "fontWeightMedium": 500,
-  button: {
-  textTransform: 'none'
-  }
-  },
-  palette: {
-      php: "red",
   }
 });
 
 function App() {
 
   return (
-  <div>
+  <div className="pageContent">
     <ThemeProvider theme={THEME}>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -39,15 +41,19 @@ function App() {
       <div className='animationContainer'>
         <HomeAnimation className='animationContainer'/>
       </div>
-      {/* <ProjectThumbnail name="Academic Paper Search"/> */}
-      <ScrollMenu className="projectCarosel">
-        <ProjectThumbnail name="Academic Paper Search" php="true" api="true" react="true"/>
-        <ProjectThumbnail name="Academic Paper Search"/>
-        <ProjectThumbnail name="Academic Paper Search"/>
-        <ProjectThumbnail name="Academic Paper Search"/>
-        <ProjectThumbnail name="Academic Paper Search"/>
-        <ProjectThumbnail name="Academic Paper Search"/>
-    </ScrollMenu>
+      <Typography sx={{width:"auto",color:"#fcfcfc",paddingTop:2, marginLeft:10, fontSize:30,'&:hover': {
+            texDecoration: "underline",
+            cursor: "pointer",
+            transition: "0.5s",
+          },}}>
+       Projects<ArrowForwardIosIcon fontSize='30'/>
+      </Typography>
+      <Box sx={{paddingTop:2, borderLeft:"3%",borderRight:"3%", display: 'flex', flexWrap: 'wrap',justifyContent: 'center',}}>
+          <ProjectThumbnail name="Large Futoshiki Generation" image={FutoshikiGeneration} java="true"software="true"/>
+          <ProjectThumbnail name="Academic Paper Search" image={AcademicPaperLibrary} php="true" api="true" react="true" js="true" sqlite="true" software="true" complete="true"/>
+          <ProjectThumbnail name="Wildfire Detection" image={WildfireDetection} php="true" mysql="true" python="true" pi="true"software="true" hardware="true" complete="true"/>
+          <ProjectThumbnail name="Handwritten Digit Recognition" image={HandwrittenDigitRecognition} matlab="true"software="true" ml="true" complete="true"/>
+      </Box>
     </ThemeProvider>
   </div>
   );
--- src/components/ProjectThumbnail.js
+++ src/components/ProjectThumbnail.js
@@ -1,114 +1,131 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Typography, Stack, Chip } from '@mui/material';
-import AcademicPaperLibrary from './../img/academicpaperlibrary.jpg'
-import { red } from '@mui/material/colors';
+import { Typography, Chip } from '@mui/material';
 
-const color = red[500];
 
 export default function ProjectThumbnail(props) {
 
-  let php = <div></div>
-  let java = <div></div>
-  let js = <div></div>
-  let matlab = <div></div>
-  let pi = <div></div>
-  let mysql = <div></div>
-  let python = <div></div>
-  let react = <div></div>
-  let ruby = <div></div>
-  let sqlite = <div></div>
-  let tailwind = <div></div>
-  let api = <div></div>
-  let hardware = <div></div>
-  let software = <div></div>
+  let tags;
+  let php;
+  let java;
+  let js;
+  let matlab;
+  let pi;
+  let mysql;
+  let python;
+  let react;
+  let ruby;
+  let sqlite;
+  let api;
+  let ml;
+  let tailwind;
+  let hardware;
+  let software;
+  let complete = <Chip label="In Progress" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#ff6f00'}}/>;
 
   if(props.php === "true"){
-    php = <Chip label="PHP" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#9978ab'}}/>
+    php= <Chip label="PHP" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#9978ab'}}/>
   }
   if(props.java === "true"){
-    java = <Chip label="Java" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#ed8932'}}/>
+    java= <Chip label="Java" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#ed8932'}}/>
   }
   if(props.js === "true"){
-    js = <Chip label="JS" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#fff70d'}}/>
+    js = <Chip label="JS" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#fff70d'}}/>
   }
   if(props.matlab === "true"){
-    matlab = <Chip label="MATLAB" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#5b68fc'}}/>
+    matlab = <Chip label="MATLAB" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#5b68fc'}}/>
   }
   if(props.pi === "true"){
-    pi = <Chip label="Raspberry Pi" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#e87168'}}/>
+    pi = <Chip label="Raspberry Pi" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#e87168'}}/>
   }
   if(props.mysql === "true"){
-    mysql = <Chip label="MySQL" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#52a3ff'}}/>
+    mysql = <Chip label="MySQL" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#52a3ff'}}/>
   }
   if(props.python === "true"){
-    python = <Chip label="Python" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#253e8f', color:'#fcfcfc'}}/>
+    python = <Chip label="Python" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#253e8f', color:'#fcfcfc'}}/>
   }
   if(props.react === "true"){
-    react = <Chip label="ReactJS" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#4aedff'}}/>
+    react = <Chip label="ReactJS" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#4aedff'}}/>
   }
   if(props.ruby === "true"){
-    ruby = <Chip label="Ruby" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#fa3725'}}/>
+    ruby = <Chip label="Ruby" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#fa3725'}}/>
   }
   if(props.sqlite === "true"){
-    sqlite = <Chip label="SQLite" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#5ea1ff'}}/>
+    sqlite = <Chip label="SQLite" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#5ea1ff'}}/>
   }
   if(props.api === "true"){
-    api = <Chip label="API" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#12fc6c'}}/>
+    api= <Chip label="API" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#12fc6c'}}/>
   }
   if(props.tailwind === "true"){
-    tailwind = <Chip label="Tailwind" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#9ef2ff'}}/>
+    tailwind= <Chip label="Tailwind" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#9ef2ff'}}/>
   }
   if(props.hardware === "true"){
-    hardware = <Chip label="Hardware" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#333333', color:'#fcfcfc'}}/>
+    hardware = <Chip label="Hardware" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#333333', color:'#fcfcfc'}}/>
   }
   if(props.software === "true"){
-    software = <Chip label="Software" sx={{marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#dbdbdb'}}/>
+    software = <Chip label="Software" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#dbdbdb'}}/>
+  }
+  if(props.complete === "true"){
+    complete = <Chip label="Complete" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#00ff08'}}/>
+  }
+  if(props.ml === "true"){
+    ml = <Chip label="Machine Learning" sx={{fontSize: 18, marginLeft:1,marginRight:1,marginBottom:0.5, backgroundColor:'#d457d2'}}/>
   }
   
   return (
-    <div>
-    <Box className='ProjectThumbnailContainer'
-      sx={{
-        width: 400,
-        backgroundColor: '#2E3B55',
-        borderRadius: '50px 50px 10px 10px',
-        paddingTop:10,
-        verticalAlign: 'text-bottom',
-        marginLeft:3,
-        marginRight:3,
-      }}
-    >
-      <Box className='ProjectThumbnailImage'>
-        <img src={AcademicPaperLibrary}></img>
-      </Box>
-      <Box className='ProjectThumbnailText'>
-        <Typography 
+    <Box sx={{marginBottom:5}}>
+      <Box className='ProjectThumbnailContainer'
         sx={{
-          fontSize: 40
-        }}>
-          {props.name}
-        </Typography>
-      </Box>
-      <Box className='ProjectThumbnailTags' >
-        <div sx={{ display: 'inline' }}>
-          {php}
-          {java}
-          {js}
-          {matlab}
-          {pi}
-          {mysql}
-          {python}
-          {react}
-          {ruby}
-          {sqlite}
-          {api}
-          {tailwind}
-          {hardware}
-          {software}
-        </div>
+          width: 400,
+          height:400,
+          backgroundColor: '#2E3B55',
+          borderRadius: '0px 0px 10px 10px',
+          verticalAlign: 'text-bottom',
+          marginLeft:3,
+          marginRight:3,
+          boxShadow: 10,
+          '&:hover': {
+            backgroundColor: '#485d87', 
+            cursor: "pointer",
+            transition: "0.5s",
+          },
+        }}
+      >
+        <Box className='ProjectThumbnailImage'>
+          <img src={props.image}></img>
+        </Box>
+        <Box className='ProjectThumbnailText'>
+          <Typography 
+          sx={{
+            fontSize: 30,
+            padding:1.5,
+            color:"#fcfcfc",
+            textAlign: "center"
+          }}>
+            {props.name}
+          </Typography>
+        </Box>
+        <Box className='ProjectThumbnailTags' >
+          <div className="tags" sx={{ display: 'inline' }}>
+            {php}
+            {java}
+            {js}
+            {matlab}
+            {pi}
+            {mysql}
+            {python}
+            {react}
+            {ruby}
+            {sqlite}
+            {api}
+            {ml}
+            {tailwind}
+            {hardware}
+            {software}
+            {complete}
+          </div>
+        </Box>
       </Box>
     </Box>
-  </div>
   );
 }
